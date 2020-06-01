4cd6fba594c9eedeb04c6b7e2b04e6c8da224c61
Alan Martins <alanhorcrux@gmail.com>
2020-06-01 20:08:37 -0300

    Adicionado pacote entity inMemories

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -25,6 +25,9 @@ public class CategoryController : ControllerBase
     [Route("")]
     public async Task<ActionResult<Category>> Post ([FromBody]Category model)
     {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+            
         return Ok(model);
     }
 
@@ -32,10 +35,13 @@ public class CategoryController : ControllerBase
     [Route("{id:int}")]
     public async Task<ActionResult<Category>> Put(int id, [FromBody]Category model)
     {
-        if(model.Id == id)
-            return model;
+        if(id != model.Id)
+            return NotFound(new {message = "Categoria não encontrada!"});
 
-        return NotFound();;
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(model);;
     }
 
     [HttpDelete]
